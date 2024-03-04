79038790b4c0650903f44782ce1a1461829b6640
Guilherme Chaves Sena <114080615+GuiiiChaves@users.noreply.github.com>
2024-03-04 18:25:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Define a função para alterar a imagem com base no tipo de dispositivo
-  function changeImageBasedOnDevice() {
-      // Detecta se o usuário está em um dispositivo móvel
-      var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-      // Define o URL da imagem com base no tipo de dispositivo
-      const imageUrl = isMobile ? "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-1.jpg" : "https://s1.1zoom.me/b5050/194/Coast_Little_girls_Clouds_532086_1920x1080.jpg";
-
-      const dynamicImage = document.getElementById("dynamicImage");
-
-      // Define o URL da imagem
-      dynamicImage.src = imageUrl;
-  }
-
-  // Chama a função para alterar a imagem com base no tipo de dispositivo
-  changeImageBasedOnDevice();
-
-  // Define uma lista de URLs das imagens para desktops
-  const desktopImageUrls = [
-      "img/teste1.jpg",
-      "img/teste2.jpg",
-      "img/imagem3.jpg"
-      // Adicione mais URLs conforme necessário
-  ];
-
-  // Define uma lista de URLs das imagens para mobile
-  const mobileImageUrls = [
-      "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-1.jpg",
-      "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-27022024-mob-2.jpg",
-      "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-4-ajuste.jpg"
-      // Adicione mais URLs conforme necessário
-  ];
-
-  const dynamicImage = document.getElementById("dynamicImage");
-  let currentIndex = 0;
-
-  // função para trocar a imagem
-  function changeImage() {
-      // Verifica se é um desktop ou dispositivo móvel
-      var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-      const imageUrls = isMobile ? mobileImageUrls : desktopImageUrls;
-
-      currentIndex = (currentIndex + 1) % imageUrls.length;
-      dynamicImage.src = imageUrls[currentIndex];
-  }
-
-  // Trocar imagem a cada 5 segundos
-  setInterval(changeImage, 5000);
-});
-
-document.querySelectorAll('.nav-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-      e.preventDefault(); // Evita que o link redirecione
-
-      // Obtém o ID do destino do link
-      const targetId = this.getAttribute('href').substring(1);
-
-      // Rola para o elemento com o ID correspondente
-      document.getElementById(targetId).scrollIntoView({ 
-          behavior: 'smooth' 
-      });
+    // Define a função para alterar a imagem com base no tipo de dispositivo
+    function changeImageBasedOnDevice() {
+        // Detecta se o usuário está em um dispositivo móvel
+        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
+        // Define o URL da imagem com base no tipo de dispositivo
+        const imageUrl = isMobile ? "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-1.jpg" : "https://s1.1zoom.me/b5050/194/Coast_Little_girls_Clouds_532086_1920x1080.jpg";
+  
+        const dynamicImage = document.getElementById("dynamicImage");
+  
+        // Define o URL da imagem
+        dynamicImage.src = imageUrl;
+    }
+  
+    // Chama a função para alterar a imagem com base no tipo de dispositivo
+    changeImageBasedOnDevice();
+  
+    // Define uma lista de URLs das imagens para desktops
+    const desktopImageUrls = [
+        "img/teste1.jpg",
+        "img/teste2.jpg",
+        "img/imagem3.jpg"
+        // Adicione mais URLs conforme necessário
+    ];
+  
+    // Define uma lista de URLs das imagens para mobile
+    const mobileImageUrls = [
+        "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-1.jpg",
+        "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-27022024-mob-2.jpg",
+        "https://www.iplace.com.br/file/general/iplaceb2c-home-cartaohoje-23022024-mob-4-ajuste.jpg"
+        // Adicione mais URLs conforme necessário
+    ];
+  
+    const dynamicImage = document.getElementById("dynamicImage");
+    let currentIndex = 0;
+  
+    // função para trocar a imagem
+    function changeImage() {
+        // Verifica se é um desktop ou dispositivo móvel
+        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        const imageUrls = isMobile ? mobileImageUrls : desktopImageUrls;
+  
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        dynamicImage.src = imageUrls[currentIndex];
+    }
+  
+    // Trocar imagem a cada 5 segundos
+    setInterval(changeImage, 5000);
   });
-});
-
-      });
+  
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Evita que o link redirecione
+  
+        // Obtém o ID do destino do link
+        const targetId = this.getAttribute('href').substring(1);
+  
+        // Rola para o elemento com o ID correspondente
+        document.getElementById(targetId).scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    });
   });
-});
